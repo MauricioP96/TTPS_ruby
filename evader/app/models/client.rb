e0f9a3e188f3569cfe1f9da3640ed23ce8d3d3b9
MauricioP96 <mauriciop_96@live.com>
2015-12-16 19:55:30 -0300

    arreglando cosas del modificar2

--- evader/app/models/client.rb
+++ evader/app/models/client.rb
@@ -8,10 +8,10 @@ class Client < ActiveRecord::Base
         									message: "only allows letters" }
 	validates :birthdate, presence: true  #ver si necesito verificar q sea un string valido como fecha DD/MM/YYYY
 	validates :gender, presence: true,inclusion: { in: %w(male female),message: " is not a valid gender"}
-	validates :dni, presence: true,numericality: { only_integer: true },length: { maximum: 8 },uniqueness:{}
+	validates :dni, presence: true,numericality: { only_integer: true },length: { maximum: 8 }
 	validates :cu_type, presence: true,inclusion: { in: %w(cuit cuil),message: " is not a valid cuit/cuil type"}
 	validates :cu_value, presence: true,format:{with: /[\d{2}]+\-[\d{8}]+\-[\d{1}]/,
-      									message: "not cuit/l format" },uniqueness:{}
+      									message: "not cuit/l format" },
     accepts_nested_attributes_for :contacts
     def actualizar_contactos(cont)
     	contacts.destroy_all
